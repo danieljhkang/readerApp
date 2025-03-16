ab6de4b1c3b17576bc9040b43d5c4dc7b65f9cf5
Dan <dkang.jh@gmail.com>
2025-03-15 20:18:53 -0400

    pages initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    </>
+    <div className="App"> 
+      <Routes>
+        <Route path="/" element={<Login />}/>
+        <Route path="/register" element={<Register />}/>
+      </Routes>
+    </div>
   )
 }
 
