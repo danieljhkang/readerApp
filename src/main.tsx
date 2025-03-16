06550d04bfa57bb20e42ccb1612d6565782052ba
Dan <dkang.jh@gmail.com>
2025-03-15 20:20:11 -0400

    root changes in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')!).render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
-)
+);
